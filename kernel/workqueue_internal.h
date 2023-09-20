939ebbbe83b6dd62808b4861ad61474631014317
Marty Leisner <maleisner@gmail.com>
2023-09-19 21:22:29 -0400

    fixed include workqueue_internal.h

--- kernel/workqueue_internal.h
+++ kernel/workqueue_internal.h
@@ -73,8 +73,8 @@ static inline struct worker *current_wq_worker(void)
  * Scheduler hooks for concurrency managed workqueue.  Only to be used from
  * sched/ and workqueue.c.
  */
-void wq_worker_waking_up(struct task_struct *task, int cpu);
-struct task_struct *wq_worker_sleeping(struct task_struct *task);
+void wq_worker_waking_up(struct task_struct *task);
+void wq_worker_sleeping(struct task_struct *task);
 work_func_t wq_worker_last_func(struct task_struct *task);
 
 #endif /* _KERNEL_WORKQUEUE_INTERNAL_H */
